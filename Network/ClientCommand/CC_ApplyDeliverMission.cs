fc8196c614cb1878ae70ff25bf9e5fc0877a5176
HeBomou <paakmau@outlook.com>
2019-05-12 20:35:57 +0800

    修改为Deliver任务

--- Network/ClientCommand/CC_ApplyDeliverMission.cs
+++ Network/ClientCommand/CC_ApplyDeliverMission.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 using LiteNetLib.Utils;
 
 namespace MirRemake {
-    class CC_ApplyFinishMission : IClientCommand {
-        public NetworkToServerDataType m_DataType { get { return NetworkToServerDataType.APPLY_FINISH_MISSION; } }
+    class CC_ApplyDeliverMission : IClientCommand {
+        public NetworkToServerDataType m_DataType { get { return NetworkToServerDataType.APPLY_DELIVER_MISSION; } }
         public void Execute(NetDataReader reader, int netId) {
             short missionId = reader.GetShort ();
             SM_ActorUnit.s_instance.CommandFinishMission (netId, missionId);
