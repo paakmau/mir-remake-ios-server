3204df1c0d8ac0634c13c6252be0e3c71dd9b9ca
tdycwym <2369351080@qq.com>
2019-05-27 03:37:24 +0800

    finished some interfaces

--- Data/IDS_Character.cs
+++ Data/IDS_Character.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
-
+using LitJson;
+using System;
+using System.IO;
 namespace MirRemakeBackend.Data {
     interface IDS_Character {
         /// <summary>
@@ -8,6 +10,47 @@ namespace MirRemakeBackend.Data {
         /// 第二维是不同的等级, 等级有序递增  
         /// </summary>
         /// <returns></returns>
-        DO_Character[][] GetAllCharacter ();
+        DO_Character[][] GetAllCharacter ();//done
+    }
+    class IDS_CharacterImpl : IDS_Character{
+        private static JsonData s_characterDatas;
+        private DO_Character[][] res;
+        public IDS_CharacterImpl(){
+            string jsonFile = File.ReadAllText("D_Character.json");
+            s_characterDatas = JsonMapper.ToObject(jsonFile);
+            res = new DO_Character[4][];
+            for(int i = 0; i < 4; i++) {
+                res[i] = new DO_Character[100];
+            }
+            for(int occupation = 0; occupation < 4; occupation++) { 
+                for(int level = 1; level <= 100; level++) {
+                    DO_Character character = new DO_Character();
+                    int i = occupation*100 + level - 1;
+                    character.m_occupation=
+                        (OccupationType)Enum.Parse(typeof(OccupationType),s_characterDatas[i]["Occupation"].ToString());
+                    character.m_level = (short)level;
+                    character.m_upgradeExperienceInNeed =
+                        int.Parse(s_characterDatas[i]["ExperienceInNeed"].ToString());
+                    character.m_mainAttributeArr = new ValueTuple<ActorUnitMainAttributeType,int>[s_characterDatas[i]["MainAttributionTable"].Count];
+                    for(int durex = 0; durex < s_characterDatas[i]["MainAttributionTable"].Count; durex++) {
+                        character.m_mainAttributeArr[i] = new ValueTuple<ActorUnitMainAttributeType, int>
+                            ((ActorUnitMainAttributeType)Enum.Parse(typeof(ActorUnitMainAttributeType),s_characterDatas[i]["MainAttributionTable"][durex].ToString().Split(' ')[0]),int.Parse(s_characterDatas[i]["MainAttributionTable"][durex].ToString().Split(' ')[1]));
+                    }
+                    character.m_concreteAttributeArr = new ValueTuple<ActorUnitConcreteAttributeType,int>[s_characterDatas[i]["ConcreteAttributionTable"].Count];
+                    for (int durex = 0; durex < s_characterDatas[i]["ConcreteAttributionTable"].Count; durex++)
+                    {
+                        character.m_concreteAttributeArr[i] = new ValueTuple<ActorUnitConcreteAttributeType, int>
+                            ((ActorUnitConcreteAttributeType)Enum.Parse(typeof(ActorUnitConcreteAttributeType), s_characterDatas[i]["ConcreteAttributionTable"][durex].ToString().Split(' ')[0]), int.Parse(s_characterDatas[i]["ConcreteAttributionTable"][durex].ToString().Split(' ')[1]));
+                    }
+                    character.m_mainAttrPointNumber = short.Parse(s_characterDatas[i]["GiftPointNumber"].ToString());
+                    res[occupation][level] = character;
+                }
+            }
+
+        }
+        public DO_Character[][] GetAllCharacter() {
+            return res;
+        }
+
     }
 }
--- Data/IDS_Status.cs
+++ Data/IDS_Status.cs
@@ -1,7 +1,41 @@
 using System.Collections.Generic;
+using System;
+using LitJson;
+using System.IO;
 
 namespace MirRemakeBackend.Data {
     interface IDS_Status {
         DO_Status[] GetAllStatus ();
     }
+    class IDS_StatusImpl : IDS_Status {
+        private static JsonData s_statusDatas;
+        private DO_Status[] res;
+        public IDS_StatusImpl() {
+            string jsonFile = File.ReadAllText("D_Character.json");
+            s_statusDatas = JsonMapper.ToObject(jsonFile);
+            res = new DO_Status[s_statusDatas.Count];
+            for(int i = 0; i < res.Length; i++) {
+                DO_Status status = new DO_Status();
+                status.m_statusId = short.Parse(s_statusDatas[i]["StatusID"].ToString());
+                status.m_type = (StatusType)Enum.Parse(typeof(StatusType), s_statusDatas[i]["Type"].ToString());
+                status.m_specialAttributeArr = new ActorUnitSpecialAttributeType[s_statusDatas[i]["SpecialAttributes"].Count];
+                for(int m = 0; m < s_statusDatas[i]["SpecialAttributes"].Count; m++) {
+                    status.m_specialAttributeArr[i] = (ActorUnitSpecialAttributeType)Enum.Parse
+                        (typeof(ActorUnitSpecialAttributeType),s_statusDatas["SpecialAttributes"][m].ToString());
+                }
+                status.m_affectAttributeArr = new ValueTuple<ActorUnitConcreteAttributeType, int>[s_statusDatas[i]["AttributeAttachArray"].Count];
+                for(int m = 0; m < s_statusDatas[i]["AttributeAttachArray"].Count; m++) {
+                    status.m_affectAttributeArr[m] = new ValueTuple<ActorUnitConcreteAttributeType,int>
+                        ((ActorUnitConcreteAttributeType)Enum.Parse(typeof(ActorUnitConcreteAttributeType),s_statusDatas[i]["AttributeAttachArray"][m].ToString().Split(' ')[0]),
+                        int.Parse(s_statusDatas[i]["AttributeAttachArray"].ToString().Split(' ')[1]));
+                }
+                res[i] = status;
+            }
+
+
+
+        }
+        public DO_Status[] GetAllStatus() {
+            return res;
+        }
 }
