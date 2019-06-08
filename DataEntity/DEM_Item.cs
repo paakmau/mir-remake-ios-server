33768c501f442711f82fe32d648dcc09c9a2916f
HeBomou <paakmau@outlook.com>
2019-06-08 18:21:23 +0800

    修改接口后充血

--- DataEntity/DEM_Item.cs
+++ DataEntity/DEM_Item.cs
@@ -12,18 +12,21 @@ namespace MirRemakeBackend.DataEntity {
         private Dictionary<short, DE_EquipmentData> m_equipmentDict = new Dictionary<short, DE_EquipmentData> ();
         private Dictionary<short, DE_GemData> m_gemDict = new Dictionary<short, DE_GemData> ();
         public DEM_Item (IDS_Item itemDs) {
-            var itemDoArr = itemDs.GetAllItem ();
             var consumableDoArr = itemDs.GetAllConsumable ();
             var equipmentDoArr = itemDs.GetAllEquipment ();
             var gemDoArr = itemDs.GetAllGem ();
-            foreach (var itemDo in itemDoArr)
-                m_itemDict.Add (itemDo.m_itemId, new DE_Item (itemDo));
-            foreach (var consumDo in consumableDoArr)
-                m_consumableDict.Add (consumDo.m_itemId, new DE_ConsumableData(consumDo));
-            foreach (var equipDo in equipmentDoArr)
-                m_equipmentDict.Add (equipDo.m_itemId, new DE_EquipmentData(equipDo));
-            foreach (var gemDo in gemDoArr)
-                m_gemDict.Add (gemDo.m_itemId, new DE_GemData(gemDo));
+            foreach (var itemDo in consumableDoArr) {
+                m_itemDict.Add (itemDo.Item1.m_itemId, new DE_Item (itemDo.Item1));
+                m_consumableDict.Add (itemDo.Item1.m_itemId, new DE_ConsumableData(itemDo.Item2));
+            }
+            foreach (var itemDo in equipmentDoArr) {
+                m_itemDict.Add (itemDo.Item1.m_itemId, new DE_Item (itemDo.Item1));
+                m_equipmentDict.Add (itemDo.Item1.m_itemId, new DE_EquipmentData(itemDo.Item2));
+            }
+            foreach (var itemDo in gemDoArr) {
+                m_itemDict.Add (itemDo.Item1.m_itemId, new DE_Item (itemDo.Item1));
+                m_gemDict.Add (itemDo.Item1.m_itemId, new DE_GemData(itemDo.Item2));
+            }
         }
         public DE_Item GetItemById (short itemId) {
             DE_Item res = null;
