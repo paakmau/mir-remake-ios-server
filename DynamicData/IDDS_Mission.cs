4e97d7fba73331c6498d35c20ebd1bae46526d6b
forsakenspirit <2369351080@qq.com>
2019-06-09 20:17:58 +0800

    changed inerface of missions

--- DynamicData/IDDS_Mission.cs
+++ DynamicData/IDDS_Mission.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 namespace MirRemakeBackend.DynamicData {
     interface IDDS_Mission {
         List<DDO_Mission> GetMissionListByCharacterId (int charId);
-        void InsertMission (DDO_Mission ddo, int charId);
-        void UpdateMission (DDO_Mission ddo, int charId);
+        void InsertMission (DDO_Mission ddo);
+        void UpdateMission (DDO_Mission ddo);
         void DeleteMission (short missionId, int charId);
     }
 }
