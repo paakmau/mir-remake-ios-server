d02cec9eebea6f29876e58205498887a6a499b40
HeBomou <paakmau@outlook.com>
2019-06-09 20:43:03 +0800

    删除远古注释

--- DynamicData/IDDS_Character.cs
+++ DynamicData/IDDS_Character.cs
@@ -11,26 +11,4 @@ namespace MirRemakeBackend.DynamicData {
         DDO_Character GetCharacterById (int characterId);
         void UpdateCharacter (DDO_Character charObj);
     }
-    //SELECT LAST_INSERT_ID()
-
-    /*class IDDS_CharacterImpl : IDDS_Character { 
-        /*public int CreateCharacter(OccupationType occupation) {
-            string cmd;
-            DataSet ds = new DataSet();
-            DataTable dt = new DataTable();
-            DataRowCollection dataRowCollection;
-            cmd = "insert into character values (null,"+occupation.ToString()+",1,0,0";
-            string database = "legend";
-            SQLPool pool = new SQLPool(sqlConfig);
-            pool.ExecuteSql(database, cmd);
-            cmd = "select last_insert_id()";
-            pool.ExecuteSql(database, cmd,ds);
-            dt = ds.Tables["character"];
-            dataRowCollection = dt.Rows;
-            return int.Parse(dataRowCollection[0]["characterid"].ToString());
-        }
-        public DDO_Character GetCharacterById(int characterId) {
-            return null;
-        }
-    }*/
 }
