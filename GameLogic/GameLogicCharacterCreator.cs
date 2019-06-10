647ea511e996189ada21459839f35fabb51a3ba9
HeBomou <paakmau@outlook.com>
2019-06-10 23:42:47 +0800

    添加了创建角色

--- GameLogic/GameLogicCharacterCreator.cs
+++ GameLogic/GameLogicCharacterCreator.cs
@@ -9,14 +9,24 @@ namespace MirRemakeBackend.GameLogic {
         private INetworkService m_netService;
         private IDDS_Character m_charDds;
         private IDDS_Skill m_skillDds;
-        public GameLogicCharacterCreator (IDDS_Character charDds, IDDS_Skill skillDds, INetworkService ns) {
+        private IDDS_Item m_itemDds;
+        public GameLogicCharacterCreator (IDDS_Character charDds, IDDS_Skill skillDds, IDDS_Item itemDds, INetworkService ns) {
             m_netService = ns;
         }
         public void CommandCreateCharacter (OccupationType ocp) {
+            // 角色 dds
             int charId = m_charDds.CreateCharacter (ocp);
+            // 技能 dds
             var skillIdList = EM_Skill.s_instance.GetSkillIdListByOccupation (ocp);
             for (int i=0; i<skillIdList.Count; i++)
                 m_skillDds.InsertSkill (new DDO_Skill (skillIdList[i], charId, 0, 0));
+            // 背包和仓库 dds TODO: 写成配置文件
+            short bagSize = 20;
+            short storeHouseSize = 50;
+            for (short i=0; i<bagSize; i++)
+                m_itemDds.InsertItem (new DDO_Item (-1, -1, charId, 0, ItemPlace.BAG, i));
+            for (short i=0; i<storeHouseSize; i++)
+                m_itemDds.InsertItem (new DDO_Item (-1, -1, charId, 0, ItemPlace.STORE_HOUSE, i));
         }
     }
 }
