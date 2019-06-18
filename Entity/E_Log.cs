4591650e29ae2aa3ff8348ba517d44cf3824dda9
HeBomou <paakmau@outlook.com>
2019-06-18 16:01:10 +0800

    彻底完成log模块

--- Entity/E_Log.cs
+++ Entity/E_Log.cs
@@ -1,7 +1,7 @@
 namespace MirRemakeBackend.Entity {
     enum GameLogType : byte {
-        MONSTER_KILL,
-        SKILL_LEVEL_UP,
+        KILL_MONSTER,
+        LEVEL_UP_SKILL,
         LEVEL_UP,
         TALK_TO_NPC,
         GAIN_ITEM
@@ -10,13 +10,55 @@ namespace MirRemakeBackend.Entity {
         public abstract GameLogType m_LogType { get; }
         public abstract void Reset (int parm1, int parm2, int parm3);
     }
-    class E_MonsterKillLog : E_Log {
-        public override GameLogType m_LogType { get { return GameLogType.MONSTER_KILL; } }
+    class E_KillMonsterLog : E_Log {
+        public override GameLogType m_LogType { get { return GameLogType.KILL_MONSTER; } }
         public int m_killerNetId;
         public short m_monId;
         public override void Reset (int parm1, int parm2, int parm3) {
             m_killerNetId = parm1;
             m_monId = (short) parm2;
+        }
+    }
+    class E_LevelUpSkillLog : E_Log {
+        public override GameLogType m_LogType { get { return GameLogType.LEVEL_UP_SKILL; } }
+        public int m_netId;
+        public short m_skillId;
+        public short m_skillLv;
+        public override void Reset (int parm1, int parm2, int parm3) {
+            m_netId = parm1;
+            m_skillId = (short) parm2;
+            m_skillLv = (short) parm3;
+        }
+    }
+    class E_LevelUpLog : E_Log {
+        public override GameLogType m_LogType { get { return GameLogType.LEVEL_UP; } }
+        public int m_netId;
+        public short m_lv;
+        public override void Reset (int parm1, int parm2, int parm3) {
+            m_netId = parm1;
+            m_lv = (short) parm2;
+        }
+    }
+    class E_TalkToNpcLog : E_Log {
+        public override GameLogType m_LogType { get { return GameLogType.TALK_TO_NPC; } }
+        public int m_netId;
+        public short m_npcId;
+        public short m_misId;
+        public override void Reset (int parm1, int parm2, int parm3) {
+            m_netId = parm1;
+            m_npcId = (short) parm2;
+            m_misId = (short) parm3;
+        }
+    }
+    class E_GainItemLog : E_Log {
+        public override GameLogType m_LogType { get { return GameLogType.GAIN_ITEM; } }
+        public int m_netId;
+        public short m_itemId;
+        public short m_num;
+        public override void Reset (int parm1, int parm2, int parm3) {
+            m_netId = parm1;
+            m_itemId = (short) parm2;
+            m_num = (short) parm3;
         }
     }
 }
--- GameLogic/GL_Log.cs
+++ GameLogic/GL_Log.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Numerics;
-using MirRemakeBackend.DataEntity;
-using MirRemakeBackend.DynamicData;
 using MirRemakeBackend.Entity;
 using MirRemakeBackend.Network;
-using MirRemakeBackend.Util;
 
 namespace MirRemakeBackend.GameLogic {
     /// <summary>
@@ -20,7 +14,8 @@ namespace MirRemakeBackend.GameLogic {
         public override void Tick (float dT) { }
         public override void NetworkTick () { }
         public void NotifyLog (GameLogType type, int parm1, int parm2, int parm3) {
-            // EM_Log.s_instance.
+            var logs = EM_Log.s_instance.GetRawLogsCurTick ();
+            logs.Add (EM_Log.s_instance.CreateLog (type, parm1, parm2, parm3));
         }
     }
 }
