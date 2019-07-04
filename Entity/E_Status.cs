fc932624390bd00e895f1cc2e79c2f02f05aecf3
HeBomou <paakmau@outlook.com>
2019-07-05 00:26:34 +0800

    complete EM_Status

--- Entity/E_Status.cs
+++ Entity/E_Status.cs
@@ -4,23 +4,24 @@ using MirRemakeBackend.Util;
 
 namespace MirRemakeBackend.Entity {
     abstract class E_Status {
-        public DE_Status m_dataEntity;
+        public DE_Status m_statusDe;
         public abstract StatusType m_Type { get; }
         public int m_casterNetId;
-        public short m_id;
+        public short m_Id { get { return m_statusDe.m_id; } }
         public float m_value;
         private float m_durationTime;
         public MyTimer.Time m_endTime;
-        public void Reset (DE_Status de, float value, float durationTime, int casterNetId) {
-            m_dataEntity = de;
-            m_id = de.m_id;
+        public void Reset (DE_Status de) {
+            m_statusDe = de;
+        }
+        public void ResetValues (float value, float durationTime, int casterNetId) {
             m_value = value;
             m_durationTime = durationTime;
             m_endTime = MyTimer.s_CurTime.Ticked (m_durationTime);
             m_casterNetId = casterNetId;
         }
         public NO_Status GetNo () {
-            return new NO_Status (m_id, m_value, m_durationTime);
+            return new NO_Status (m_Id, m_value, m_durationTime);
         }
     }
     class E_ChangeHpStatus : E_Status {
@@ -30,15 +31,17 @@ namespace MirRemakeBackend.Entity {
         public override StatusType m_Type { get { return StatusType.CHANGE_MP; } }
     }
     class E_ConcreteAttributeStatus : E_Status {
+        private DE_ConcreteAttributeStatus m_conAttrDe;
         public override StatusType m_Type { get { return StatusType.CONCRETE_ATTRIBUTE; } }
-        public void Reset (DE_Status statusDe, DE_ConcreteAttributeStatus casDe, float value, float durationTime, int casterNetId) {
-            base.Reset (statusDe, value, durationTime, casterNetId);
+        public void ResetConAttrData (DE_ConcreteAttributeStatus casDe) {
+            m_conAttrDe = casDe;
         }
     }
     class E_SpecialAttributeStatus : E_Status {
+        private DE_SpecialAttributeStatus m_spAttrDe;
         public override StatusType m_Type { get { return StatusType.SPECIAL_ATTRIBUTE; } }
-        public void Reset (DE_Status statusDe, DE_SpecialAttributeStatus sasDe, float value, float durationTime, int casterNetId) {
-            base.Reset (statusDe, value, durationTime, casterNetId);
+        public void ResetSpAttrData (DE_SpecialAttributeStatus sasDe) {
+            m_spAttrDe = sasDe;
         }
     }
 }
--- Entity/EntityManager/EM_Status.cs
+++ Entity/EntityManager/EM_Status.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using MirRemakeBackend.DataEntity;
 using MirRemakeBackend.Util;
 
@@ -7,26 +9,72 @@ namespace MirRemakeBackend.Entity {
         private class StatusFactory {
             #region StatusInitializer
             private interface IStatusInitializer {
-                
+                StatusType m_StatusType { get; }
+                void ResetStatusData (DE_Status de, DEM_Status dem, E_Status resStatus);
+            }
+            private class SI_ChangeHp : IStatusInitializer {
+                public StatusType m_StatusType { get { return StatusType.CHANGE_HP; } }
+                public void ResetStatusData (DE_Status de, DEM_Status dem, E_Status resStatus) {
+                    resStatus.Reset (de);
+                }
+            }
+            private class SI_ChangeMp : IStatusInitializer {
+                public StatusType m_StatusType { get { return StatusType.CHANGE_MP; } }
+                public void ResetStatusData (DE_Status de, DEM_Status dem, E_Status resStatus) {
+                    resStatus.Reset (de);
+                }
+            }
+            private class SI_ConcreteAttribute : IStatusInitializer {
+                public StatusType m_StatusType { get { return StatusType.CONCRETE_ATTRIBUTE; } }
+                public void ResetStatusData (DE_Status de, DEM_Status dem, E_Status resStatus) {
+                    resStatus.Reset (de);
+                    ((E_ConcreteAttributeStatus) resStatus).ResetConAttrData (dem.GetConAttrStatusById (de.m_id));
+                }
+            }
+            private class SI_SpecialAttribute : IStatusInitializer {
+                public StatusType m_StatusType { get { return StatusType.SPECIAL_ATTRIBUTE; } }
+                public void ResetStatusData (DE_Status de, DEM_Status dem, E_Status resStatus) {
+                    resStatus.Reset (de);
+                    ((E_SpecialAttributeStatus) resStatus).ResetSpAttrData (dem.GetSpAttrStatusById (de.m_id));
+                }
             }
             #endregion
+            private DEM_Status m_dem;
             private const int c_poolSize = 2000;
-            private Dictionary<StatusType, ObjectPool> m_poolDict;
-            public StatusFactory () {
+            private Dictionary<StatusType, ObjectPool> m_poolDict = new Dictionary<StatusType, ObjectPool> ();
+            private Dictionary<StatusType, IStatusInitializer> m_initializerDict = new Dictionary<StatusType, IStatusInitializer> ();
+            public StatusFactory (DEM_Status dem) {
+                m_dem = dem;
                 m_poolDict.Add (StatusType.CHANGE_HP, new ObjectPool<E_ChangeHpStatus> (c_poolSize));
                 m_poolDict.Add (StatusType.CHANGE_MP, new ObjectPool<E_ChangeMpStatus> (c_poolSize));
                 m_poolDict.Add (StatusType.CONCRETE_ATTRIBUTE, new ObjectPool<E_ConcreteAttributeStatus> (c_poolSize));
                 m_poolDict.Add (StatusType.SPECIAL_ATTRIBUTE, new ObjectPool<E_SpecialAttributeStatus> (c_poolSize));
+
+                // 实例化所有ClientCommand接口的实现类
+                var type = typeof (IStatusInitializer);
+                var implTypes = AppDomain.CurrentDomain.GetAssemblies ().SelectMany (s => s.GetTypes ()).Where (p => p.IsClass && type.IsAssignableFrom (p));
+                foreach (var implType in implTypes) {
+                    IStatusInitializer impl = type.GetConstructor (Type.EmptyTypes).Invoke (null) as IStatusInitializer;
+                    m_initializerDict.Add (impl.m_StatusType, impl);
+                }
             }
             public E_Status GetInstance (DE_Status de) {
                 var res = m_poolDict[de.m_type].GetInstanceObj () as E_Status;
+                m_initializerDict[de.m_type].ResetStatusData (de, m_dem, res);
                 return res;
+            }
+            public void RecycleInstance (E_Status obj) {
+                m_poolDict[obj.m_Type].RecycleInstance (obj);
             }
         }
         public static EM_Status s_instance;
         private DEM_Status m_dem;
+        private StatusFactory m_fact;
         private Dictionary<int, List<E_Status>> m_statusListDict = new Dictionary<int, List<E_Status>> ();
-        public EM_Status (DEM_Status dem) { m_dem = dem; }
+        public EM_Status (DEM_Status dem) {
+            m_dem = dem;
+            m_fact = new StatusFactory (dem);
+        }
         public void InitCharacterStatus (int netId) {
             m_statusListDict.TryAdd (netId, new List<E_Status> ());
         }
