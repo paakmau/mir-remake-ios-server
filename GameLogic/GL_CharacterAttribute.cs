dd449487c226a7aaa1a457ece35932a8122890ac
forsakenspirit <2369351080@qq.com>
2019-07-06 01:28:53 +0800

    [add] finished main point strategies

--- GameLogic/GL_CharacterAttribute.cs
+++ GameLogic/GL_CharacterAttribute.cs
@@ -94,6 +94,14 @@ namespace MirRemakeBackend.GameLogic {
         private void MainPointToConAttr (E_Character charObj) {
             // TODO: 根据角色属性点 直接计算 具体属性
             // charObj.SetMainPointConAttr (ActorUnitConcreteAttributeType.ATTACK, charObj.m_Strength * 233);
+            charObj.SetMainPointConAttr (ActorUnitConcreteAttributeType.ATTACK, (int)(charObj.m_Strength*0.55));
+            charObj.SetMainPointConAttr (ActorUnitConcreteAttributeType.ATTACK, (int)(charObj.m_Agility*0.2));
+            charObj.SetMainPointConAttr (ActorUnitConcreteAttributeType.CRITICAL_BONUS, (int)(charObj.m_Agility*0.05));
+            charObj.SetMainPointConAttr (ActorUnitConcreteAttributeType.CRITICAL_BONUS, (int)(charObj.m_Agility*0.0015));
+            charObj.SetMainPointConAttr (ActorUnitConcreteAttributeType.MAX_MP, (int)(charObj.m_Intelligence*2));
+            charObj.SetMainPointConAttr (ActorUnitConcreteAttributeType.DELTA_HP_PER_SECOND, (int)(charObj.m_Spirit*0.05));
+            charObj.SetMainPointConAttr (ActorUnitConcreteAttributeType.MAGIC, (int)(charObj.m_Intelligence*0.5));
+            charObj.SetMainPointConAttr (ActorUnitConcreteAttributeType.DELTA_MP_PER_SECOND, (int)(charObj.m_Spirit*0.05));
         }
     }
 }
