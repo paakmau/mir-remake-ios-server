75cb7cef5744d38cd2fae223bd2dbd048cc92a55
forsakenspirit <2369351080@qq.com>
2019-07-05 20:29:41 +0800

    s

--- Data/IDS_GroundItemMap.cs
+++ Data/IDS_GroundItemMap.cs
@@ -13,4 +13,18 @@ namespace MirRemakeBackend.Data {
         ValueTuple<short, Vector2>[] GetAllGroundItemRespawnPosition ();
     }
     // TODO:
+    class DS_GroundItemMapImpl : IDS_GroundItemMap{
+        private JsonData s_itemPosDatas;
+        public ValueTuple<short,Vector2>[] GetAllGroundItemRespawnPosition(){
+            string jsonFile = File.ReadAllText("Data/D_ItemMap.json");
+            s_itemPosDatas = JsonMapper.ToObject(jsonFile);
+            ValueTuple<short,Vector2>[] res=new ValueTuple<short,Vector2>[s_itemPosDatas.Count];
+            for(int i=0;i<s_itemPosDatas.Count;i++){
+                res[i]=new ValueTuple<short,Vector2>(short.Parse(s_itemPosDatas["ID"].ToString()),
+                    new Vector2(float.Parse(s_itemPosDatas[i]["x"].ToString()),
+                        float.Parse(s_itemPosDatas[i]["y"].ToString())));
+            }
+            return res; 
+        }
+    }
 }
