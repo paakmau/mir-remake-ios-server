565a8695ea4805b028f992523da16f85be4d4ed6
forsakenspirit <2369351080@qq.com>
2019-07-07 22:06:12 +0800

    [change] some strategies

--- Data/IDS_GroundItemMap.cs
+++ Data/IDS_GroundItemMap.cs
@@ -2,7 +2,6 @@ using System;
 using System.IO;
 using System.Numerics;
 using LitJson;
-
 namespace MirRemakeBackend.Data {
     interface IDS_GroundItemMap {
         /// <summary>
@@ -16,12 +15,15 @@ namespace MirRemakeBackend.Data {
         private JsonData s_itemPosDatas;
         public ValueTuple<short,Vector2>[] GetAllGroundItemRespawnPosition(){
             string jsonFile = File.ReadAllText("Data/D_ItemMap.json");
+            Random ran=new Random();
             s_itemPosDatas = JsonMapper.ToObject(jsonFile);
-            ValueTuple<short,Vector2>[] res=new ValueTuple<short,Vector2>[s_itemPosDatas.Count];
+            ValueTuple<short,Vector2>[] res=new ValueTuple<short,Vector2>[s_itemPosDatas.Count*5];
             for(int i=0;i<s_itemPosDatas.Count;i++){
-                res[i]=new ValueTuple<short,Vector2>(short.Parse(s_itemPosDatas["ID"].ToString()),
-                    new Vector2(float.Parse(s_itemPosDatas[i]["x"].ToString()),
-                        float.Parse(s_itemPosDatas[i]["y"].ToString())));
+                for (int j=0;j<5;j++){
+                    res[i*5+j]=new ValueTuple<short,Vector2>(short.Parse(s_itemPosDatas["ID"].ToString()),
+                        new Vector2(float.Parse(s_itemPosDatas[i]["x"].ToString())+(float)ran.Next(-10000,10000)/10000,
+                            float.Parse(s_itemPosDatas[i]["y"].ToString())+(float)ran.Next(-10000,10000)/10000));
+                }
             }
             return res; 
         }
