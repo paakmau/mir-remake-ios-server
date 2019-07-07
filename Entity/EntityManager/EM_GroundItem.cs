dfff4f42b1528d23ea24dc4dd9ef9786fa37e3e1
HeBomou <paakmau@outlook.com>
2019-07-07 23:38:38 +0800

    正在地面物品

---
+++ Entity/EntityManager/EM_GroundItem.cs
@@ -0,0 +1,8 @@
+namespace MirRemakeBackend.Entity {
+    class EM_GroundItem : EntityManagerBase {
+        public static EM_GroundItem s_instance;
+        public void NotifyDropItem (E_Item item) {
+            
+        }
+    }
+}
