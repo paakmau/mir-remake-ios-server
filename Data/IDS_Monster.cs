084292a5c02838d0e9904a1d065b0cb4ac59bab8
forsakenspirit <2369351080@qq.com>
2019-07-15 12:58:14 +0800

    1

--- Data/IDS_Monster.cs
+++ Data/IDS_Monster.cs
@@ -36,11 +36,11 @@ namespace MirRemakeBackend.Data {
                     int num=int.Parse(s_monsterDatas[durex]["ConcreteAttributionTable"][x].ToString().Split(' ')[1]);
                     if(type==ActorUnitConcreteAttributeType.ATTACK || type==ActorUnitConcreteAttributeType.MAGIC){
                         monster.m_attrArr[x] = new ValueTuple<ActorUnitConcreteAttributeType, int>
-                            (type, int.Parse(s_monsterDatas[durex]["ConcreteAttributionTable"][x].ToString().Split(' ')[1])/2);
+                            (type, num/2);
                     }
                     else if(type==ActorUnitConcreteAttributeType.MAX_HP){
                         monster.m_attrArr[x] = new ValueTuple<ActorUnitConcreteAttributeType, int>
-                            (type, num/2);
+                            (type, (int)(num*0.4));
                     }
                     else{
                         monster.m_attrArr[x] = new ValueTuple<ActorUnitConcreteAttributeType, int>
