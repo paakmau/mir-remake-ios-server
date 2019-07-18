34b07bb80ea429f8c80085b239a858104ba78f50
HeBomou <paakmau@outlook.com>
2019-07-18 12:03:58 +0800

    add anotation

--- DynamicData/IDDS_Character.cs
+++ DynamicData/IDDS_Character.cs
@@ -3,10 +3,9 @@ namespace MirRemakeBackend.DynamicData {
         /// <summary>
         /// 创建一个角色
         /// </summary>
-        /// <returns>角色的id</returns>
+        /// <returns>角色的id, 若因为重名或其他原因无法创建返回 -1</returns>
         int CreateCharacter (int playerId, OccupationType occupation, string name);
         DDO_Character GetCharacterById (int characterId);
-        DDO_Character GetCharacterByName (string name);
         void UpdateCharacter (DDO_Character charObj);
         DDO_Character[] GetCharacterByPlayerId (int playerId);
     }
