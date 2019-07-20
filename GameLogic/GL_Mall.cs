f0855333f8cdd270fcd2d042be7075a7e5d6c90d
HeBomou <paakmau@outlook.com>
2019-07-21 00:45:54 +0800

    finish buy multiple item in mall

--- GameLogic/GL_Mall.cs
+++ GameLogic/GL_Mall.cs
@@ -15,18 +15,19 @@ namespace MirRemakeBackend.GameLogic {
                 mallClassNoList.Add (mallClassEn.Current.Value.GetNo ());
             m_networkService.SendServerCommand (SC_SendShoppingMall.Instance (netId, mallClassNoList));
         }
-        public void CommandBuyItemInShoppingMall (int netId, int mallItemId, short itemNum, CurrencyType cyType) {
+        public void CommandBuyItemInShoppingMall (int netId, int mallItemId, short num, CurrencyType cyType) {
             E_Character charObj = EM_Unit.s_instance.GetCharacterByNetworkId (netId);
             E_MallItem itemObj = EM_MallItem.s_instance.GetMallItemByMallItemId (mallItemId);
             if (charObj == null || itemObj == null) return;
             long needCy = cyType == CurrencyType.VIRTUAL ? itemObj.m_VirtualCyPrice : itemObj.m_ChargeCyPrice;
             if (needCy == -1) return;
+            needCy *= num;
             long charCy = cyType == CurrencyType.VIRTUAL ? charObj.m_virtualCurrency : charObj.m_chargeCurrency;
             if (charCy < needCy) return;
             // 扣钱
             GL_CharacterAttribute.s_instance.NotifyUpdateCurrency (charObj, cyType, -needCy);
             // gain item
-            GL_Item.s_instance.NotifyCharacterGainItem (netId, charObj.m_characterId, itemObj.m_ItemId, 1);
+            GL_Item.s_instance.NotifyCharacterGainItem (netId, charObj.m_characterId, itemObj.m_ItemId, num);
         }
     }
 }
