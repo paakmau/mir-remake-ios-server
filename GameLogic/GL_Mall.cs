4ec28711261c3137b47cf52e651ddd7ba7c5f504
HeBomou <paakmau@outlook.com>
2019-07-20 14:29:39 +0800

    finish shopping mall buy item

--- GameLogic/GL_Mall.cs
+++ GameLogic/GL_Mall.cs
@@ -16,7 +16,18 @@ namespace MirRemakeBackend.GameLogic {
             m_networkService.SendServerCommand (SC_SendShoppingMall.Instance (netId, mallClassNoList));
         }
         public void CommandBuyItemInShoppingMall (int netId, int mallItemId, CurrencyType cyType) {
-            // TODO: 购买商城物品
+            E_Character charObj = EM_Unit.s_instance.GetCharacterByNetworkId (netId);
+            E_MallItem itemObj = EM_MallItem.s_instance.GetMallItemByMallItemId (mallItemId);
+            if (charObj == null || itemObj == null) return;
+            long needCy = cyType == CurrencyType.VIRTUAL ? itemObj.m_VirtualCyPrice : itemObj.m_ChargeCyPrice;
+            if (needCy == -1) return;
+            long charCy = cyType == CurrencyType.VIRTUAL ? charObj.m_virtualCurrency : charObj.m_chargeCurrency;
+            if (charCy < needCy) return;
+            // 扣钱
+            GL_CharacterAttribute.s_instance.NotifyUpdateCurrency (charObj, cyType, -needCy);
+            // gain item
+            GL_Item.s_instance.NotifyCharacterGainItem (netId, charObj.m_characterId, new List < (short, short) > () {
+                (itemObj.m_ItemId, 1) });
         }
     }
 }
