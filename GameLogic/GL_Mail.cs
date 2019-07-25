f5c4a75bf2ffe1357603b263f4958cfdbeb66ce6
HeBomou <paakmau@outlook.com>
2019-07-26 02:00:53 +0800

    完成了mail模块

--- GameLogic/GL_Mail.cs
+++ GameLogic/GL_Mail.cs
@@ -25,7 +25,7 @@ namespace MirRemakeBackend.GameLogic {
         public void CommandApplyReadAllMail (int netId) {
             var mailList = EM_Mail.s_instance.GetAllMailByNetId (netId);
             if (mailList == null) return;
-            for (int i=0; i<mailList.Count; i++) {
+            for (int i = 0; i < mailList.Count; i++) {
                 if (mailList[i].m_isRead) continue;
                 EM_Mail.s_instance.CharacterReadMail (netId, mailList[i]);
                 m_networkService.SendServerCommand (SC_ApplySelfReadMail.Instance (netId, mailList[i].m_id));
@@ -36,24 +36,23 @@ namespace MirRemakeBackend.GameLogic {
             var charId = EM_Character.s_instance.GetCharIdByNetId (netId);
             if (mail == null || charId == -1) return;
             if (mail.m_isReceived) return;
-            var itemIdAndNumList = mail.m_itemIdAndNumList;
-            var virtualCy = mail.m_virtualCy;
-            var chargeCy = mail.m_chargeCy;
+            GL_Wallet.s_instance.NotifyUpdateVirtualCurrency (netId, charId, mail.m_virtualCy);
+            GL_Wallet.s_instance.NotifyUpdateChargeCurrency (netId, charId, mail.m_chargeCy);
+            GL_Item.s_instance.NotifyCharacterGainItems (netId, charId, mail.m_itemIdAndNumList);
             EM_Mail.s_instance.CharacterReceiveMail (netId, mail);
             m_networkService.SendServerCommand (SC_ApplySelfReceiveMail.Instance (netId, mailId));
-            GL_Wallet.s_instance.NotifyUpdateVirtualCurrency (netId, charId, virtualCy);
-            GL_Wallet.s_instance.NotifyUpdateChargeCurrency (netId, charId, chargeCy);
-            GL_Item.s_instance.NotifyCharacterGainItems (netId, charId, itemIdAndNumList);
         }
         public void CommandApplyReceiveAllMail (int netId) {
             var mailList = EM_Mail.s_instance.GetAllMailByNetId (netId);
             var charId = EM_Character.s_instance.GetCharIdByNetId (netId);
             if (mailList == null || charId == -1) return;
-            for (int i=0; i<mailList.Count; i++) {
+            for (int i = 0; i < mailList.Count; i++) {
                 if (mailList[i].m_isReceived) continue;
-                var itemIdAndNumList = mailList[i].m_itemIdAndNumList;
+                GL_Wallet.s_instance.NotifyUpdateVirtualCurrency (netId, charId, mailList[i].m_virtualCy);
+                GL_Wallet.s_instance.NotifyUpdateChargeCurrency (netId, charId, mailList[i].m_chargeCy);
+                GL_Item.s_instance.NotifyCharacterGainItems (netId, charId, mailList[i].m_itemIdAndNumList);
                 EM_Mail.s_instance.CharacterReceiveMail (netId, mailList[i]);
-                GL_Item.s_instance.NotifyCharacterGainItems (netId, charId, itemIdAndNumList);
+                m_networkService.SendServerCommand (SC_ApplySelfReceiveMail.Instance (netId, mailList[i].m_id));
             }
         }
         public void NotifyInitCharacter (int netId, int charId) {
@@ -62,7 +61,7 @@ namespace MirRemakeBackend.GameLogic {
         public void NotifyRemoveCharacter (int netId) {
             EM_Mail.s_instance.RemoveCharacter (netId);
         }
-    public void NotifySendMail (int senderCharId, string senderName, int recvNetId, int recvCharId, string title, string detail, List < (short, short) > itemIdAndNumList, long virtualCy, long chargeCy) {
+        public void NotifySendMail (int senderCharId, string senderName, int recvNetId, int recvCharId, string title, string detail, List < (short, short) > itemIdAndNumList, long virtualCy, long chargeCy) {
             EM_Mail.s_instance.SendMail (senderCharId, senderName, recvNetId, recvCharId, title, detail, itemIdAndNumList, virtualCy, chargeCy);
         }
     }
