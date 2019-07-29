c98e8b94fd47ba719a4b11bdcf53ff27192219e1
HeBomou <paakmau@outlook.com>
2019-07-29 23:24:46 +0800

    fix api

--- DynamicData/IDDS_Mail.cs
+++ DynamicData/IDDS_Mail.cs
@@ -5,7 +5,7 @@ namespace MirRemakeBackend.DynamicData {
     interface IDDS_Mail {
         List<DDO_Mail> GetAllMailByReceiverCharacterId (int charId);
         bool DeleteMailById (int id);
-        bool InsertMail (DDO_Mail mail);
+        int InsertMail (DDO_Mail mail);
         bool UpdateMailRead (int id, bool isRead);
         bool UpdateMailReceived (int id, bool isReceived);
         void DeleteMailBeforeCertainTime(DateTime time);
