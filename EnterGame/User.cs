b39bf5332222cd27a588f59d41e9f12709064237
FNhaha <584948862@qq.com>
2019-09-01 01:21:40 +0800

    更新客户端版本号和下载连接

--- EnterGame/User.cs
+++ EnterGame/User.cs
@@ -11,8 +11,8 @@ using MirRemakeBackend.Network;
 namespace MirRemakeBackend.EnterGame {
     class User {
         public static User s_instance;
-        private const string c_version = "1.00";
-        private const string c_downloadUrl = "www.baidu.com";
+        private const string c_version = "1.01";
+        private const string c_downloadUrl = "https://cloud.189.cn/t/AF3Mjyaueum2";
         private INetworkService m_netService;
         private IDDS_User m_userDds;
         private IDDS_Character m_charDds;
