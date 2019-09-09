1489090347e5e76159d813140f2b76b4f4c00bce
HeBomou <paakmau@outlook.com>
2019-09-10 00:53:41 +0800

    修复了新建角色的title任务与佩戴的bug

--- EnterGame/User.cs
+++ EnterGame/User.cs
@@ -20,11 +20,13 @@ namespace MirRemakeBackend.EnterGame {
         private IDDS_CharacterWallet m_charWalletDds;
         private IDDS_Skill m_skillDds;
         private IDDS_Mission m_misDds;
+        private IDDS_Title m_titleDds;
         private IDDS_Item m_itemDds;
         private IDDS_CombatEfct m_combatEfctDds;
         private Dictionary<OccupationType, List<short>> m_ocpSkillIdDict = new Dictionary<OccupationType, List<short>> ();
         private Dictionary<OccupationType, List<short>> m_ocpInitMisIdDict = new Dictionary<OccupationType, List<short>> ();
-        public User (IDS_Skill skillDs, IDS_Mission misDs, IDDS_User userDds, IDDS_Character charDds, IDDS_CharacterAttribute charAttrDds, IDDS_CharacterWallet charWalletDds, IDDS_Skill skillDds, IDDS_Mission misDds, IDDS_Item itemDds, IDDS_CombatEfct combatEfctDds, INetworkService ns) {
+        private List<short> m_titleMisIdList = new List<short> ();
+        public User (IDS_Skill skillDs, IDS_Mission misDs, IDDS_User userDds, IDDS_Character charDds, IDDS_CharacterAttribute charAttrDds, IDDS_CharacterWallet charWalletDds, IDDS_Skill skillDds, IDDS_Mission misDds, IDDS_Title titleDds, IDDS_Item itemDds, IDDS_CombatEfct combatEfctDds, INetworkService ns) {
             m_netService = ns;
             m_userDds = userDds;
             m_charDds = charDds;
@@ -32,6 +34,7 @@ namespace MirRemakeBackend.EnterGame {
             m_charWalletDds = charWalletDds;
             m_skillDds = skillDds;
             m_misDds = misDds;
+            m_titleDds = titleDds;
             m_itemDds = itemDds;
             m_combatEfctDds = combatEfctDds;
             // 初始技能加载
@@ -53,6 +56,10 @@ namespace MirRemakeBackend.EnterGame {
                     foreach (var ocp in ocpArr)
                         if ((ocp | mDo.m_missionOccupation) != 0)
                             m_ocpInitMisIdDict[ocp].Add (mDo.m_id);
+
+            var allTitleMis = misDs.GetAllTitleMission ();
+            foreach (var titleMis in allTitleMis)
+                m_titleMisIdList.Add (titleMis.m_id);
         }
         public void CommandConnect (int netId) {
             m_netService.SendServerCommand (SC_InitSelfNetworkId.Instance (netId, c_version, c_downloadUrl));
@@ -112,6 +119,7 @@ namespace MirRemakeBackend.EnterGame {
                 m_netService.SendServerCommand (SC_InitSelfModifyPassword.Instance (netId, false));
         }
         public void CommandCreateCharacter (int netId, int playerId, OccupationType ocp, string name) {
+
             // 角色 dds
             int charId = m_charDds.InsertCharacter (new DDO_Character (-1, playerId, ocp, name));
             if (charId == -1) {
@@ -120,14 +128,21 @@ namespace MirRemakeBackend.EnterGame {
             }
             m_charAttrDds.InsertCharacterAttribute (new DDO_CharacterAttribute (charId, 1, 0, 0, 0, 0, 0));
             m_charWalletDds.InsertCharacterWallet (new DDO_CharacterWallet (charId, 0, 0));
+
             // 技能 dds
             var skillIdList = m_ocpSkillIdDict[ocp];
             for (int i = 0; i < skillIdList.Count; i++)
                 m_skillDds.InsertSkill (new DDO_Skill (skillIdList[i], charId, 0, 0));
-            // 任务 dds
+
+            // 任务, 称号任务 dds
             var misIdList = m_ocpInitMisIdDict[ocp];
             for (int i = 0; i < misIdList.Count; i++)
                 m_misDds.InsertMission (new DDO_Mission (misIdList[i], charId, MissionStatus.ACCEPTABLE, new List<int> ()));
+            for (int i = 0; i < m_titleMisIdList.Count; i++)
+                m_misDds.InsertTitleMission (new DDO_Mission (m_titleMisIdList[i], charId, MissionStatus.ACCEPTED, new List<int> ()));
+            // 称号 dds
+            m_titleDds.InsertAttachedTitle (charId, -1);
+
             // 背包, 仓库, equipment dds
             short bagSize = 15;
             short storeHouseSize = 8;
@@ -138,6 +153,7 @@ namespace MirRemakeBackend.EnterGame {
                 m_itemDds.InsertItem (new DDO_Item (-1, -1, charId, 0, ItemPlace.STORE_HOUSE, i));
             for (short i = 0; i < eqSize; i++)
                 m_itemDds.InsertItem (new DDO_Item (-1, -1, charId, 0, ItemPlace.EQUIPMENT_REGION, i));
+
             // 战斗力排行 dds
             m_combatEfctDds.InsertMixCombatEfct (new DDO_CombatEfct (charId, 0, ocp, name, 1));
             m_netService.SendServerCommand (SC_InitSelfCreateCharacter.Instance (netId, true, charId));
