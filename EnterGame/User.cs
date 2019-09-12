5937588dc559cbe737098fd7da85ec8dee6cfb06
HeBomou <paakmau@outlook.com>
2019-09-12 16:30:33 +0800

    更新了版本号

--- EnterGame/User.cs
+++ EnterGame/User.cs
@@ -11,7 +11,7 @@ using MirRemakeBackend.Network;
 namespace MirRemakeBackend.EnterGame {
     class User {
         public static User s_instance;
-        private const string c_version = "1.10";
+        private const string c_version = "1.21";
         private const string c_downloadUrl = "https://cloud.189.cn/t/yemU3ejaqYBv";
         private INetworkService m_netService;
         private IDDS_User m_userDds;
