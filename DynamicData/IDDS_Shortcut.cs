331713165ac78afea992bce972aabb3e9d1bc308
HeBomou <paakmau@outlook.com>
2019-10-02 20:53:17 +0800

    添加了shortcut

--- DynamicData/IDDS_Shortcut.cs
+++ DynamicData/IDDS_Shortcut.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 
 namespace MirRemakeBackend.DynamicData {
     interface IDDS_Shortcut {
-        // List<>
+        List<DDO_Shortcut> GetShortcutsByCharId (int charId);
+        void InsertShortcut (DDO_Shortcut ddo);
+        void UpdateShortcutByCharIdAndPosition (DDO_Shortcut ddo);
+        void DeleteShortcutByCharId (int charId);
     }
 }
