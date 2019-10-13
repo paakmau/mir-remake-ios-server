11889e8705ae1f6a23b9cf999aecb72a0b969b85
HeBomou <paakmau@outlook.com>
2019-10-14 00:03:17 +0800

    彻底修正deploy

---
+++ publish.sh
@@ -0,0 +1,9 @@
+user="hebo"
+# pass="gfyTQL233"
+host="39.99.134.200"
+src="bin/Release/netcoreapp2.2/linux-x64/publish/"
+dest="~/"
+
+dotnet publish -c Release -r linux-x64
+cp Data/*.json $src/Data/
+scp -r $src $user@$host:$dest
