4f1c994d9a27d160e7986be5324b733780903add
HeBomou <paakmau@outlook.com>
2019-11-04 14:15:41 +0800

    修复爆红

--- DynamicData/IDDS_Alliance.cs
+++ DynamicData/IDDS_Alliance.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 namespace MirRemakeBackend.DynamicData {
     interface IDDS_Alliance {
         // 联盟
-        public int InsertAlliance (DDO_Alliance ddo);
-        public void DeleteAllianceById (int id);
-        public List<DDO_Alliance> GetAllAlliance ();
+        int InsertAlliance (DDO_Alliance ddo);
+        void DeleteAllianceById (int id);
+        List<DDO_Alliance> GetAllAlliance ();
 
         // 联盟加入申请
-        public List<DDO_AllianceApply> GetApplyByAllianceId (int allianceId);
-        public bool DeleteApplyById (int id);
-        public int InsertApply (DDO_AllianceApply ddo);
+        List<DDO_AllianceApply> GetApplyByAllianceId (int allianceId);
+        bool DeleteApplyById (int id);
+        int InsertApply (DDO_AllianceApply ddo);
 
         // 成员管理
-        public List<DDO_AllianceMember> GetMemberByAllianceId (int allianceId);
-        public bool DeleteMemberByCharId (int charId);
-        public void InsertMember (DDO_AllianceMember ddo);
-        public void UpdateMember (DDO_AllianceMember ddo);
+        List<DDO_AllianceMember> GetMemberByAllianceId (int allianceId);
+        bool DeleteMemberByCharId (int charId);
+        void InsertMember (DDO_AllianceMember ddo);
+        void UpdateMember (DDO_AllianceMember ddo);
     }
 }
