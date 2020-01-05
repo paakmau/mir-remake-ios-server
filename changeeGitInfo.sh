9957a9b1fe6e8b1666fd3a9f73ac098fc36267a8
HeBomou <paakmau@outlook.com>
2020-01-05 12:53:35 +0800

    c

--- changeeGitInfo.sh
+++ changeeGitInfo.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 git filter-branch --env-filter '
-OLD_EMAIL="529801036@qq.com"
+OLD_EMAIL="hebo@bomoudembp.lan"
 CORRECT_NAME="HeBomou"
 CORRECT_EMAIL="paakmau@outlook.com"
 if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
